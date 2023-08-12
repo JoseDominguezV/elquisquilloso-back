c5b92a15b75b4b59adcb59c5fe56ce33f55f8eaf
Paoladevelopment <paolala1017@gmail.com>
2023-08-12 01:54:05 -0500

    Configuraciones iniciales

---
+++ database/migrations/2023_08_11_212006_create_comentarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('fecha');
+            $table->text('comentario');
+            $table->foreignUuid('articulo_comentado')->constrained('articulos')->OnDelete('cascade');
+            $table->foreignId('hace_comentario')->constrained('usuarios')->OnDelete('cascade');
+            $table->foreignId('respuesta_de')->constrained('comentarios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comentarios');
+    }
+};
